fbbd143f91822beec2e2ecd7f4d4b0f8a8c63a73
r3kashi <97653516+r3kashi@users.noreply.github.com>
2023-11-07 20:13:39 +0800

    Trying to inserting student data to my database

---
+++ code.php
@@ -0,0 +1,50 @@
+/* The code you provided is a PHP script that handles the creation of a student record in a database. */
+
+
+<?php
+
+// Enable error reporting
+
+$con = mysqli_connect('localhost', 'root','', 'blog');
+
+if(isset($_POST['save_student']))
+{
+  $name = mysqli_real_escape_string($con, $_POST['name']);
+  $email = mysqli_real_escape_string($con, $_POST['email']);
+  $phone = mysqli_real_escape_string($con, $_POST['phone']);
+  $course = mysqli_real_escape_string($con, $_POST['course']);
+
+  if($name == NULL || $email == NULL || $phone == NULL || $course == NULL)
+  {
+    $res = [  
+      'status' => 422,
+      'message' => 'All fields are mandatory'
+    ];
+    echo json_encode($res);
+    return false;
+  }
+
+  $query = "INSERT INTO students (name, email, phone, course) VALUES ('$name','$email','$phone','$course')";
+  $query_run = mysqli_query($con, $query);
+
+  if($query_run)
+  { 
+    $res = [
+      'status' => 200,
+      'message' => 'Student Created Successfully'
+    ];
+    echo json_encode($res);
+    return false;
+  }
+  else
+  {
+    $res = [
+      'status' => 500,
+      'message' => 'Student Not Created'
+    ];
+    echo json_encode($res);
+    return false; 
+  }
+}
+
+?>
--- students.php
+++ students.php
@@ -9,8 +9,6 @@
   <meta name="description" content="PHP ajax crud, php mysql crud app">
   <!-- Bootstrap CSS -->
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-  <!-- Bootstrap JS Bundle with Popper -->
-  <script defer src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
   <title>PHP Ajax CRUD</title>
 </head>
 
@@ -24,8 +22,9 @@
           <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
         </div>
 
-        <form action="">
+        <form id="saveStudent">
           <div class="modal-body">
+            <div class="alert alert-warning d-none" id="errorMessage"></div>
             <div class="mb-3">
               <label for="name" class="form-label">Name</label>
               <input type="text" class="form-control" id="name">
@@ -45,10 +44,9 @@
             </div>
           </div>
         </form>
-
         <div class="modal-footer">
           <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-          <button type="button" class="btn btn-primary">Save Student</button>
+          <button type="submit" class="btn btn-primary">Save Student</button>
         </div>
       </div>
     </div>
@@ -73,6 +71,42 @@
       </div>
     </div>
   </div>
+
+
+  <!-- Bootstrap JS Bundle with Popper -->
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+  <!-- JQuery -->
+  <script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
+
+  <script>
+    $(document).on('submit', '#saveStudent', function (e) {
+      e.preventDefault(); 
+
+      var formData = new FormData(this);
+      formData.append("save_student", true);
+
+      $.ajax({
+        type: "POST",
+        url: "code.php",
+        data: formData,
+        processData: false,
+        contentType: false,
+        success: function (response) {
+
+          var res = jQuery.parseJSON(response);
+          if(res.status == 422) {
+            $('#errorMessage').removeClass('d-none');
+            $('#errorMessage').text(res.message); 
+          }else if(res.status == 200){
+
+            $('#errorMessage').addClass('d-none');
+            $('#studentAddModal').modal('hide');
+            $('#saveStudent')[0].reset();
+          }
+        }
+      });
+    });
+  </script>
 
 </body>
 
