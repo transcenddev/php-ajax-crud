6436d0b10fd16a83c87e95b57b9de5c4ad8ae65d
r3kashi <97653516+r3kashi@users.noreply.github.com>
2023-11-07 15:29:48 +0800

    Added students.php

---
+++ students.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <!-- Required meta tags -->
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta name="author" content="transcenddev, Reymar V. Mirante">
+  <meta name="description" content="php ajax crud, php mysql crud app">
+  <!-- Bootstrap CSS -->
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+  <!-- Bootstrap JS Bundle with Popper -->
+  <script defer src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
+  <title>PHP AJAX CRUD</title>
+</head>
+
+<body>
+  <h1>Hello World!</h1>
+</body>
+
+</html>
